5ea42bff18f52bd81ce8ab19faaa1419b12fe4cb
Gabriel Fernandes <166411483+Gabriel-Fern@users.noreply.github.com>
2025-05-17 10:52:11 -0300

    Update Main.java

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -1,7 +1,46 @@
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	
+	Scanner input = new Scanner (System.in);
+	
+	float salada = 20f;
+	float hamburguer = 8.50f;
+	float milkshake = 10f;
+	float coca = 4f;
+	
+	System.out.println("Insira a quantidade de dinheiro que você esta disposto a gastar");
+	float dinheiro = input.nextFloat(); 
+	
+	System.out.println("\n Salada: "+ salada + "$");
+	    System.out.println("\n Insira a quantidade de saladas desejada:");
+        float quantidadeSalada = input.nextFloat() * salada;
+	        
+	System.out.println("\n Hamburguer: "+ hamburguer + "$");
+	    System.out.println("\n Insira a quantidade de Hamburguer desejada:");
+        float quantidadeHamburguer = input.nextFloat() * hamburguer;
+
+	System.out.println("\n Coca-Cola: "+ coca + "$");
+	    System.out.println("\n Insira a quantidade de Coca Cola desejada:");
+        float quantidadeCoca = input.nextFloat() * coca;
+
+	System.out.println("\n Milkshake: "+ milkshake + "$");
+	    System.out.println("\n Insira a quantidade de Milkshake desejada:");
+        float quantidadeMilkshake = input.nextFloat() * milkshake;
+		
+    float carrinho = quantidadeMilkshake + quantidadeCoca + quantidadeHamburguer + quantidadeSalada;
+	
+	System.out.println("A conta é de "+ carrinho + "$.");
+	
+	if (dinheiro > carrinho){
+	    System.out.println("Bom apetite!");
+	} 	
+		
+	else{
+	    System.out.println("Você é pobre e passou fome");
+	}
+	
 	}
 }
